4a4f117664deb392546fc79a846050031699919d
Binh Tran <tranthaibinh111@yahoo.com>
2023-07-12 18:48:46 +0700

    temporary

---
+++ Models/Ghtk/PostOffice/Filter/PostOfficeFilterModel.cs
@@ -0,0 +1,14 @@
+namespace GhtkCore.Models.Ghtk
+{
+  public class PostOfficeFilterModel
+  {
+    public string warehouseCode { get; set; }
+    public string contactName { get; set; }
+    public string address { get; set; }
+    public string province { get; set; }
+    public string district { get; set; }
+    public string ward { get; set; }
+    public string street { get; set; }
+  }
+}
+
---
+++ Models/Ghtk/PostOffice/Get/PostOfficeModel.cs
@@ -0,0 +1,37 @@
+#region Package (third-party)
+using AutoMapper;
+#endregion
+
+#region GHTK
+// Paramters
+using GhtkCore.Models.Ghtk;
+#endregion
+
+namespace GhtkCore.Models.Ghtk
+{
+  public class PostOfficeModel
+  {
+    public string warehouseCode { get; set; }
+    public string contactName { get; set; }
+    public string address { get; set; }
+    public string province { get; set; }
+    public string district { get; set; }
+    public string ward { get; set; }
+    public string street { get; set; }
+
+    #region Map
+    public static PostOfficeModel map(PersonalFilterModel source)
+    {
+      #region Config
+      var config = new MapperConfiguration(cfg =>
+        cfg.CreateMap<PersonalFilterModel, PersonalModel>()
+      );
+      var mapper = new Mapper(config);
+      #endregion
+
+      return mapper.Map<PersonalFilterModel, PostOfficeModel>(source);
+    }
+    #endregion
+  }
+}
+
