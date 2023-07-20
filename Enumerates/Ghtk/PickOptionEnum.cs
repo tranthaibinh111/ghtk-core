a4fe53a3761d85cb858555ca4a52e268157890d6
Binh Tran <tranthaibinh111@yahoo.com>
2023-07-20 18:10:36 +0700

    Update Enumerates

---
+++ Enumerates/Ghtk/PickOptionEnum.cs
@@ -0,0 +1,15 @@
+namespace GhtkCore.Enumerates.Ghtk
+{
+  public class PickOptionEnum
+  {
+    /// <summary>
+    /// Lấy hàng tại shop
+    /// </summary>
+    public const string Shop = "cod";
+    /// <summary>
+    /// Shop sẽ gửi tại bưu cục gần nhất
+    /// </summary>
+    public const string Post = "post";
+  }
+}
+
---
+++ Enumerates/Ghtk/PickWorkShiftEnum.cs
@@ -0,0 +1,19 @@
+namespace GhtkCore.Enumerates.Ghtk
+{
+  public enum PickWorkShiftEnum
+  {
+    /// <summary>
+    /// Buổi tối
+    /// </summary>
+    Evening = 3,
+    /// <summary>
+    /// Buồi chiều
+    /// </summary>
+    Afternoon = 2,
+    /// <summary>
+    /// Buồi sáng
+    /// </summary>
+    Morning = 1,
+  }
+}
+
